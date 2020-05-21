8b8e34618a8f6a2ef57779ed08f2f89264379282
Fragoso, André <andre.fragoso95@gmail.com>
2020-05-21 11:51:16 -0300

    [routas] - criando os componentes de rotas

--- src/routes.js
+++ src/routes.js
@@ -7,9 +7,8 @@ export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path='/' exact component= {LoginPage}/>
-                <Route path='/Home' component= {HomePage}/>
-
+                <Route path='/' exact component={LoginPage} />
+                <Route path='/Home' component={HomePage} />
             </Switch>
         </BrowserRouter>
     )
