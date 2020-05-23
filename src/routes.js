e897bc63bdf12792e150e2f9e1a04a40864335b1
Fragoso, André <andre.fragoso95@gmail.com>
2020-05-23 07:01:04 -0300

    [routas] - adicionando novas rotas

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,11 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import LoginPage from './pages/loginPage';
 import HomePage from './pages/homePage';
+import ChatPage from './pages/chatPage';
+import EntregaAtividadePage from './pages/entregaAtividadePage';
+import ListaAtividadesPage from './pages/listaAtividadesPage';
+import MaterialDidaticoPage from './pages/materialDidaticoPage';
+import PlanejamentoPage from './pages/planejamentoPage';
 import React from 'react';
 
 export default function Routes() {
@@ -9,6 +14,11 @@ export default function Routes() {
             <Switch>
                 <Route path='/' exact component={LoginPage} />
                 <Route path='/Home' component={HomePage} />
+                <Route path='/Chat' component={ChatPage} />
+                <Route path='/EntregaAtividade' component={EntregaAtividadePage} />
+                <Route path='/ListaAtividade' component={ListaAtividadesPage} />
+                <Route path='/MaterialDidatico' component={MaterialDidaticoPage} />
+                <Route path='/Planejamento' component={PlanejamentoPage} />
             </Switch>
         </BrowserRouter>
     )
